363242ced4dae754f8cfd4ee25da4891ef29c723
Tirtharaj <tirtharajjana615@gmail.com>
2021-10-12 16:33:43 +0530

    show files

--- client/src/components/Home/subcomponents/file/File.jsx
+++ client/src/components/Home/subcomponents/file/File.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import { Typography } from '@material-ui/core';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+
+
 
 const File = ({ file }) => {
-    // console.log(file);
+    console.log(file);
+    const size = (file.size / 1024 / 1024).toFixed(3)
+
     return (
-        <div>
-            {file.name}
-        </div>
+        <>
+
+            <Card sx={{ maxWidth: 200 }} elevation={6} >
+                <CardContent>
+
+                    <Typography noWrap color='primary' variant="subtitle1" component="div">
+                        {file.name}
+                    </Typography>
+                    <Typography varient="subtitle2" sx={{ mb: 1.5 }} color="text.secondary">
+                        {`Size: ${size} MB`}
+                    </Typography>
+                    <Typography variant="body2">
+                        {`Type: ${file.fileType}`}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </>
     )
 }
 
 export default File
+
--- client/src/components/Home/subcomponents/file/Files.jsx
+++ client/src/components/Home/subcomponents/file/Files.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import File from './File';
+import { Grid } from '@mui/material';
+
 
 const Files = () => {
     const { fileDetails } = useSelector(state => state.folderDetails);
@@ -8,12 +10,18 @@ const Files = () => {
     return (
         <>
             <p>Files</p>
-            {fileDetails &&
-                fileDetails.map((file, id) => (
-                    <File key={id} file={file} />
-                    // console.log(file);
-                ))
-            }
+            <Grid container alignItems='center' spacing={3} >
+                {fileDetails &&
+                    fileDetails.map((file, id) => (
+                        <Grid key={file._id} item xs={6} sm={6} md={2} lg={2} >
+
+                            <File file={file} />
+                        </Grid>
+                        // console.log(file);
+                    ))
+                }
+            </Grid>
+
         </>
     )
 }
